1b19efd19910ff4d9c09505ac86ba9a4aa3db199
Niketa Chaudhari <niketac@excellonsoft.com>
2023-02-16 12:10:27 +0530

    changes

--- backend/src/helper/response.ts
+++ backend/src/helper/response.ts
@@ -1,15 +1,15 @@
-import {Request, Response} from 'express';
+import {Request, response, Response} from 'express';
 
-export function sendSuccess(request: Request, Message = 'Request Successfull', data?:any) {
+export function sendSuccess(request: Request,response: Response, Message = 'Request Successfull', data?:any) {
 
-    let response = { StatusCode: 200, IsRequestSuccessfull: true,Message:Message, Data:data }
+    response.send({ StatusCode: 200, IsRequestSuccessfull: true,Message:Message, Data:data })
 
 }
 
 export function sendError(request: Request, response :Response,errorCode:number,errorMgs:string) {
 
-    let Response = { StatusCode: errorCode, IsRequestSuccessfull: false, Message: errorMgs}
-    response.send(Response)
+   // let Response = 
+    response.status(400).send({IsRequestSuccessfull: false,errorMgs:errorMgs})
 
 }
    
--- backend/src/utils/sendmail.ts
+++ backend/src/utils/sendmail.ts
@@ -1,32 +1,42 @@
 import nodemailer from "nodemailer";
 import {Request,Response} from 'express'
 import { sendError, sendSuccess } from "../helper/response";
-
 export const sendEmail = async (req:Request,res:Response,authuser:any,mailObj:any) => {
+  console.log("authuser@@@@@@inside mail send",authuser)
   const transporter = nodemailer.createTransport({
-    host: authuser.SMTPHost,
-    secure: authuser.smtpsecure,
-    port: authuser.SMTPPort,
+    host: authuser[0].SMTPHost,
+    secure: authuser[0].smtpsecure == 'SSL'? true : false,
+    port: authuser[0].SMTPPort, 
     auth: {
-      user: authuser.username,
-      pass: authuser.password,
+      user: authuser[0].username,
+      pass: authuser[0].password,
     }
   });
-
+console.log("transportertransporter",transporter)
   const options = {
-    from: authuser.username,
+    from: authuser[0].username,
     to: mailObj.email,
     // replyTo: reply_to,
     subject: mailObj.subject,
-    html:mailObj.body
+    html:'<h1>,</h1><p>You have successfully created.</p>'
   };
 
+
+  transporter.verify(function (error, success) {
+    if (error) {
+      console.log("error in verify smtp",error);
+    } else {
+      console.log("Server is ready to take our messages");
+    }
+  });
   // Send Email
   transporter.sendMail(options, (err:any, info:any) => {
     if (err) {
+      console.log("errrrr",err)
         sendError(req,res,500,err)
     } else {
-      sendSuccess(req,'email sent successfully',info);
+      console.log("info",info)
+      sendSuccess(req,res,'email sent successfully',info);
     }
   });
 };
